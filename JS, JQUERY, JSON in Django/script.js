a6609280052c0f1499de9c1abf5b6e5c10d559ab
Douglas Carvalho <carvalhodouglaspereira@gmail.com>
2022-02-12 17:46:50 -0300

    Funções em JS.

--- JS, JQUERY, JSON in Django/script.js
+++ JS, JQUERY, JSON in Django/script.js
@@ -1,3 +1,8 @@
 alert("I'm here!")
 document.write("<p>Hello Word</p>")
-console.log("Test consol")
+console.log("Prod = "+product(4,5))
+
+function product(a,b) {
+    value = a + b;
+    return value;
+}
